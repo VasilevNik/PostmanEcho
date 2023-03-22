a5d542b34dd9a3b018405390290314a332b91a65
VasilevNik <heshelbe@inbox.ru>
2023-03-22 23:13:17 +0300

    Change PostmanEchoTest

--- src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
+++ src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("RUB"))
+                .body("data", equalTo("USD"))
         ;
     }
 }
